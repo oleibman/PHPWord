117c8ea9b606897b96afbd7374c906a35c191407
oleibman <10341515+oleibman@users.noreply.github.com>
2023-10-21 21:22:09 -0700

    Eliminate Coverage Upload

--- tests/PhpWordTests/Writer/Word2007/Part/DocumentTest.php
+++ tests/PhpWordTests/Writer/Word2007/Part/DocumentTest.php
@@ -408,7 +408,13 @@ class DocumentTest extends \PHPUnit\Framework\TestCase
         // behind
         $element = $doc->getElement('/w:document/w:body/w:p[2]/w:r/w:pict/v:shape');
         $style = $element->getAttribute('style');
-        self::assertRegExp('/z\-index:\-[0-9]*/', $style);
+        if (method_exists(self::class, 'assertMatchesRegularExpression')) {
+            self::assertMatchesRegularExpression('/z\-index:\-[0-9]*/', $style);
+        } elseif (method_exists(self::class, 'assertRegExp')) {
+            self::assertRegExp('/z\-index:\-[0-9]*/', $style);
+        } else {
+            self::fail('Unsure how to test regexp');
+        }
 
         // square
         $element = $doc->getElement('/w:document/w:body/w:p[4]/w:r/w:pict/v:shape/w10:wrap');
