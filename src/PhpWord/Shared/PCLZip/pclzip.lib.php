e096c0e240fe127de78a433de5163b5bd8241eb2
oleibman <10341515+oleibman@users.noreply.github.com>
2026-09-07 16:44:46 -0700

    More 8.6

--- src/PhpWord/Shared/PCLZip/pclzip.lib.php
+++ src/PhpWord/Shared/PCLZip/pclzip.lib.php
@@ -291,7 +291,7 @@ class PclZip
             $v_size--;
 
             // ----- Look for first arg
-            if ((is_integer($v_arg_list[0])) && ($v_arg_list[0] > 77000)) {
+            if ((is_int($v_arg_list[0])) && ($v_arg_list[0] > 77000)) {
 
                 // ----- Parse the options
                 $v_result = $this->privParseOptions($v_arg_list, $v_size, $v_options, array(
@@ -466,7 +466,7 @@ class PclZip
             $v_size--;
 
             // ----- Look for first arg
-            if ((is_integer($v_arg_list[0])) && ($v_arg_list[0] > 77000)) {
+            if ((is_int($v_arg_list[0])) && ($v_arg_list[0] > 77000)) {
 
                 // ----- Parse the options
                 $v_result = $this->privParseOptions($v_arg_list, $v_size, $v_options, array(
@@ -712,7 +712,7 @@ class PclZip
             $v_arg_list = func_get_args();
 
             // ----- Look for first arg
-            if ((is_integer($v_arg_list[0])) && ($v_arg_list[0] > 77000)) {
+            if ((is_int($v_arg_list[0])) && ($v_arg_list[0] > 77000)) {
 
                 // ----- Parse the options
                 $v_result = $this->privParseOptions($v_arg_list, $v_size, $v_options, array(
@@ -1446,7 +1446,7 @@ class PclZip
 
                     // ----- Check the value
                     $v_value = $p_options_list[$i + 1];
-                    if ((!is_integer($v_value)) || ($v_value < 0)) {
+                    if ((!is_int($v_value)) || ($v_value < 0)) {
                         PclZip::privErrorLog(PCLZIP_ERR_INVALID_OPTION_VALUE, "Integer expected for option '" . PclZipUtilOptionText($p_options_list[$i]) . "'");
 
                         return PclZip::errorCode();
@@ -1604,7 +1604,7 @@ class PclZip
 
                         // ----- Parse items
                         $v_work_list = explode(",", $p_options_list[$i + 1]);
-                    } elseif (is_integer($p_options_list[$i + 1])) {
+                    } elseif (is_int($p_options_list[$i + 1])) {
                         $v_work_list[0] = $p_options_list[$i + 1] . '-' . $p_options_list[$i + 1];
                     } elseif (is_array($p_options_list[$i + 1])) {
                         $v_work_list = $p_options_list[$i + 1];
@@ -1903,7 +1903,7 @@ class PclZip
                     break;
 
                 case PCLZIP_ATT_FILE_MTIME:
-                    if (!is_integer($v_value)) {
+                    if (!is_int($v_value)) {
                         PclZip::privErrorLog(PCLZIP_ERR_INVALID_ATTRIBUTE_VALUE, "Invalid type " . gettype($v_value) . ". Integer expected for attribute '" . PclZipUtilOptionText($v_key) . "'");
 
                         return PclZip::errorCode();
